c1f87c7a2b7f82b1ffd0cea34a966c3dbd13bd1c
Roman Pelikh <pelikh.roman@gmail.com>
2020-04-14 19:10:06 -0300

    fixed styles splash

--- Server/controller.js
+++ Server/controller.js
@@ -1,9 +1,10 @@
 const server = require("./mqttHelper").mqttHelper
 
-const { acitons: { CALCULATE_SCORE, START_THE_GAME, GET_RANDOM_BUSH, CLICK_VIRUS, ON_USER_JOIN, GAME_OVER, ALL_USER_JOINED, ON_ERROR, ON_USER_LEAVE } } = require("./actions")
+const { acitons: { CALCULATE_SCORE, START_THE_GAME, GET_RANDOM_BUSH, ON_USER_JOIN, GAME_OVER, ALL_USER_JOINED, ON_ERROR, ON_USER_LEAVE } } = require("./actions")
 
 
 const RANDOM_NERDS_CLIENT = "randomnerdsClient";
+const WINNING_SCORE = 10;
 
 const sendMessageObject = (obj) => {
     server.publish(RANDOM_NERDS_CLIENT, JSON.stringify(obj))
@@ -16,19 +17,7 @@ const gameState = {
 
 let users = [];
 
-function getRandomBush(bshSize) {
-    return Math.floor(Math.random() * bshSize);
-}
 
-function generateRandomTime(min = 300, max = 1000) {
-    return Math.floor(Math.random() * (max - min) + min)
-}
-
-function calculateScore({ userId }) {
-    let user = users.find(u => u.userId == userId)
-    user.userScore = user.userScore + 1;
-    return { user, users }
-}
 
 const reducer = ({ action, payload }) => {
     switch (action) {
@@ -36,7 +25,6 @@ const reducer = ({ action, payload }) => {
             if (!canStartGame(users)) {
                 return { action: ON_ERROR, payload: "The Game cannot be started. Please wait for second user to join" }
             }
-
             gameState.gameIsPlaying = true;
             //SET SCORE TO 0 FOR EACH
             users.forEach(us => us.userScore = 0)
@@ -48,7 +36,7 @@ const reducer = ({ action, payload }) => {
         case CALCULATE_SCORE:
             if (gameState.gameIsPlaying) {
                 const usersWithScore = calculateScore(payload);
-                if (usersWithScore.user.userScore == 3) {
+                if (usersWithScore.user.userScore == WINNING_SCORE) {
                     sendMessageObject({ action: GAME_OVER, payload: { gameIsPlaying: false, user: usersWithScore.user } })
                 }
                 return { action: CALCULATE_SCORE, payload: usersWithScore }
@@ -56,6 +44,7 @@ const reducer = ({ action, payload }) => {
                 break;
             }
         case ON_USER_JOIN:
+            console.log(`JOINED: ${payload.userId}`)
             users.push(payload);
             if (canStartGame(users)) {
                 sendMessageObject({ action: ALL_USER_JOINED, payload: true })
@@ -63,13 +52,26 @@ const reducer = ({ action, payload }) => {
             return { action: ON_USER_JOIN, payload: users }
         case ON_USER_LEAVE:
             removeUserFromList(payload);
-
             return { action: ON_USER_LEAVE, payload: users }
         default:
             return { action: "Default action" }
     }
 }
 
+
+function getRandomBush(bshSize) {
+    return Math.floor(Math.random() * bshSize);
+}
+
+function generateRandomTime(min = 300, max = 800) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function calculateScore({ userId }) {
+    let user = users.find(u => u.userId == userId)
+    user.userScore = user.userScore + 1;
+    return { user, users }
+}
 
 function canStartGame(users) {
     if (users.length >= 2) {
@@ -81,6 +83,7 @@ function canStartGame(users) {
 
 function removeUserFromList(id) {
     users = users.filter(u => u.userId != id)
+    console.log(users)
 }
 
 server.on('message', function (topic, message) {
